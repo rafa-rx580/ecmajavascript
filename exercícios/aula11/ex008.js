773c3c7f716d6bb4ccb28307e5dc408189d8a567
Rafael Gomes <156968191+rafa-rx580@users.noreply.github.com>
2025-02-12 09:40:16 -0300

    Aula 11

---
+++ exercícios/aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 60;
+console.log('Dirija sempre usando cinto de segurança!')
+console.log(`A velocidade do seu carro é ${vel} km por hora!`)
+if (vel > 100) { //condição simples
+    console.log('Você ultrapassou a velocidade permitida! MULTADO!')
+}
---
+++ exercícios/aula11/ex009.js
@@ -0,0 +1,6 @@
+var country = 'BR';
+if(country == 'BR'){
+    console.log('Você é brasileiro!')
+} else{
+    console.log('Você é estrangeiro!')
+}
