14946850b948612d0bbd2d82b9639af072a2b770
Rafael Gomes <156968191+rafa-rx580@users.noreply.github.com>
2025-02-14 09:40:16 -0300

    ex015

--- exercícios/aula013/ex 02/script2.js
+++ exercícios/aula013/ex 02/script2.js
@@ -15,26 +15,35 @@ function verificar(){
                 genero = 'homem'
                 if (idade >= 0 && idade < 13){
                     //criança
+                    img.setAttribute('src', 'mbaby.jpg')
                 } else if (idade < 22){
                     // jovem
+                    img.setAttribute('src', 'myoung.jpg')
                 } else if (idade < 60){
                     //adulto
+                    img.setAttribute('src', 'madult.jpg')
                 } else {
                     //idoso
+                    img.setAttribute('src', 'mold.jpg')
                 }
             } else{
                 genero = 'mulher'
                 if (idade >= 0 && idade < 13){
                     //criança
+                    img.setAttribute('src', 'fbaby.jpg')
                 } else if (idade < 22){
                     // jovem
+                    img.setAttribute('src', 'fyoung.jpg')
                 } else if (idade < 60){
                     //adulto
+                    img.setAttribute('src', 'fadult.jpg')
                 } else {
                     //idoso
+                    img.setAttribute('src', 'fold.jpg')
                 }
             }
-            res.innerHTML = `${genero} ${idade}`
+            res.innerHTML = `Detectamos um(a) ${genero} com ${idade} ano(s)!`
+            res.appendChild(img)
         }
     
 
