5a352203f0bfd0c6f9c66a0badf95b6bf442052e
Rafael Gomes <156968191+rafa-rx580@users.noreply.github.com>
2025-02-18 10:34:58 -0300

    exercicio contando!

--- exercícios/ex014ex/modelo/script.js
+++ exercícios/ex014ex/modelo/script.js
@@ -1,13 +1,25 @@
 function contar(){
-    var start = document.getElementById('txtstart');
-    var end = document.getElementById('txtend');
-    var step = document.getElementById('txtstep');
-    var res = document.getElementById('res')
-    var n = 0
-    var s = Number(start)
-    var e = Number(end)
-    var st = Number(step)
-    for (n = s; n <= e; n = n + st ){
-       res.innerHTML = `${n}` 
+    let start = document.getElementById('txti')
+    let end = document.getElementById('txte')
+    let step = document.getElementById('txts')
+    let res = document.getElementById('res')
+    if (start.value.length == 0 || end.value.length == 0 || step.value.length == 0){
+        window.alert('ERRO, FALTAM DADOS')
+    } else if (Number(start.value) < Number(end.value)){
+        res.innerHTML = 'Contando...'
+        let t = Number(start.value)
+        let e = Number(end.value)
+        let p = Number(step.value)
+        for(let n = t; n <= e; n += p){
+            res.innerHTML += ` ${n} `
+        }
+    } else{
+        res.innerHTML = 'Contando...'
+        let t = Number(start.value)
+        let e = Number(end.value)
+        let p = Number(step.value)
+        for (let n = t; n >= e; n -= p){
+            res.innerHTML += ` ${n} `
+        } 
     }
 }
